8be7a17590d1ec354d191eb09630752278cb3ca8
Phan Thac <phanthac.futa@gmail.com>
2024-02-21 22:42:21 +0700

    update code video 38 CRUD react

--- src/containers/System/UserManage.js
+++ src/containers/System/UserManage.js
@@ -1,20 +1,62 @@
 import React, { Component } from 'react';
 import { FormattedMessage } from 'react-intl';
 import { connect } from 'react-redux';
+import './UserManage.scss';
+import { getAllUsers } from '../../services/userService'
 class UserManage extends Component {
 
-    state = {
-
+    constructor(props) {
+        super(props);
+        this.state = {
+            arrUsers: []
+        }
     }
 
-    componentDidMount() {
-
+    async componentDidMount() {
+        let response = await getAllUsers("ALL");
+        if (response && response.errCode === 0) {
+            this.setState({
+                arrUsers: response.users
+            })
+        }
+        console.log('da get duoc data', response);
     }
 
 
     render() {
+        let arrUsers = this.state.arrUsers;
         return (
-            <div className="text-center">Manage users</div>
+            <div className="users-container">
+                <div className="title text-center">user react</div>
+                <div className="users-table mt-4">
+                    <table id="customers">
+                        <tr>
+                            <th>Email</th>
+                            <th>First Name</th>
+                            <th>Last Name</th>
+                            <th>Address</th>
+                            <th>Actions</th>
+                        </tr>
+
+                        {arrUsers && arrUsers.map((item, index) => {
+                            console.log('check data', arrUsers)
+                            return (
+                                <tr>
+                                    <td>{item.email}</td>
+                                    <td>{item.firstName}</td>
+                                    <td>{item.lastName}</td>
+                                    <td>{item.address}</td>
+                                    <td>
+                                        <button className="mx-3"><i className="fas fa-pencil-alt"></i></button>
+                                        <button><i className="fas fa-trash-alt"></i></button>
+                                    </td>
+                                </tr>
+                            )
+                        })}
+
+                    </table>
+                </div>
+            </div>
         );
     }
 
