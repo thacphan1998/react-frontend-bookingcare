b9e6bb4b746171b5629d81e444d3d68bfb97d39c
Phan Thac <phanthac.futa@gmail.com>
2024-02-28 19:03:50 +0700

    success code video 93

--- src/containers/Patient/Doctor/DoctorExtraInfor.js
+++ src/containers/Patient/Doctor/DoctorExtraInfor.js
@@ -15,10 +15,15 @@ class DoctorExtraInfor extends Component {
             extraInfor: {},
         }
     }
-    componentDidMount() {
-
-
-
+    async componentDidMount() {
+        if (this.props.doctorIdFromParent) {
+            let res = await getExtraInforDoctorById(this.props.doctorIdFromParent);
+            if (res && res.errCode === 0) {
+                this.setState({
+                    extraInfor: res.data
+                })
+            }
+        }
     }
 
     async componentDidUpdate(prevProps, prevState, snapshot) {
--- src/containers/Patient/Specialty/DetailSpecialty.js
+++ src/containers/Patient/Specialty/DetailSpecialty.js
@@ -3,12 +3,15 @@ import { connect } from "react-redux";
 import { FormattedMessage } from 'react-intl';
 import './DetailSpecialty.scss';
 import HomeHeader from '../../HomePage/HomeHeader';
+import DoctorSchedule from '../Doctor/DoctorSchedule';
+import DoctorExtraInfor from '../Doctor/DoctorExtraInfor';
+import ProfileDoctor from '../Doctor/ProfileDoctor'
 
 class DetailSpecialty extends Component {
     constructor(props) {
         super(props)
         this.state = {
-
+            arrDoctorId: [36, 37]
         }
     }
     componentDidMount() {
@@ -31,12 +34,48 @@ class DetailSpecialty extends Component {
 
 
     render() {
-
+        let { arrDoctorId } = this.state;
         return (
-            <>
+            <div className="detail-specialty-container">
                 <HomeHeader />
-                <div>hello detail specialty</div>
-            </>
+                <div className="detail-specialty-body">
+                    <div className="description-specialty">
+
+                    </div>
+
+                    {arrDoctorId && arrDoctorId.length > 0 &&
+                        arrDoctorId.map((item, index) => {
+                            return (
+
+                                <div className="each-doctor" key={index}>
+                                    <div className="left">
+                                        <div className="profile-doctor">
+                                            <ProfileDoctor
+                                                doctorId={item}
+                                                isShowDescriptionDoctor={true}
+                                            // dataScheduleTimeModal={dataScheduleTimeModal}
+                                            />
+                                        </div>
+                                    </div>
+                                    <div className="right">
+                                        <div className="doctor-schedule">
+                                            < DoctorSchedule
+                                                doctorIdFromParent={item}
+                                            />
+                                        </div>
+                                        <div className="doctor-extra-infor">
+                                            <DoctorExtraInfor
+                                                doctorIdFromParent={item}
+                                            />
+                                        </div>
+                                    </div>
+                                </div>
+
+                            )
+                        })
+                    }
+                </div>
+            </div>
 
         );
     }
